0af6afee0d0e6c443a07f5502ec31bdeb5c26710
sachin kumar <sachinroysky@gmail.com>
2024-09-20 16:22:04 +0530

    commit for assignment

---
+++ src/components/DashBoard/DashView.jsx
@@ -0,0 +1,122 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { DiCodeigniter } from "react-icons/di";
+import { AiOutlinePlus } from "react-icons/ai";
+import "./DashView.css";
+import Card from "../Card/Card";
+
+const DashView = () => {
+  const { selectedData, user } = useSelector(
+    (state) => state.SelectDataReducer
+  );
+
+  const images = {
+    progress:"in-progress.svg",
+    todo: "To-do.svg",
+    Backlog:"Backlog.svg",
+    nopriority:"No-priority.svg",
+    low:"Img - Low Priority.svg",
+    high:"Img - High Priority.svg",
+    urgent:"SVG - Urgent Priority colour.svg",
+    medium:"Img - Medium Priority.svg",
+  };
+
+   
+
+  const getImageBasedOnTitle = (title) => {
+    console.log("the title is",title);
+    switch (title!=null&&title) {
+      case "In progress":
+        return images.progress;
+      case "Todo":
+        return images.todo;
+      case "Backlog":
+        return images.Backlog;
+      case "No priority":
+        return images.nopriority;
+      case "Low":
+        return images.low;
+      case "Medium":
+        return images.medium;
+      case "High":
+        return images.high;
+      case "Urgent":
+        return images.urgent;
+     default:
+        return "https://path-to-default-image.jpg";
+    }
+  };
+
+
+  return (
+    selectedData && (
+      <div className="dashContainer" style={{ justifyContent: "space-evenly" }}>
+        {selectedData.map((elem, index) => {
+          return (
+            <>
+              <div key={index} className="dashCardContainer">
+                <div className="dashCardHeading flex-sb">
+                  <div className="leftView">
+                    {!user ? (
+                       <>
+                       <div
+                         className="imageContainer relative"
+                         style={{ width: "15px", height: "15px", display: "inline-block" }}
+                       >
+                         <img
+                           style={{
+                             width: "100%",
+                             height: "100%",
+                             borderRadius: "50%",
+                           }}
+                           src={getImageBasedOnTitle(elem[index].title)}
+                           alt={elem.title}
+                         />
+                       </div>
+                     </>
+
+                    ) : (
+                      <>
+                        <div
+                          className="imageContainer relative"
+                          style={{ width: "15px", height: "15px", display : 'inline-block' }}
+                        >
+                          <img
+                            style={{
+                              width: "100%",
+                              height: "100%",
+                              borderRadius: "50%",
+                            }}
+                            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
+                            alt="UserImage"
+                          />
+                        </div>
+                      </>
+                    )}
+                    <span>
+                      {" "}
+                      {elem[index]?.title} {elem[index]?.value?.length}
+                    </span>
+                  </div>
+                  <div className="rightView">
+                    <AiOutlinePlus />{" "}
+                    <span style={{ letterSpacing: "2px" }}>...</span>
+                  </div>
+                </div>
+                <div className="dashList flex-gap-10">
+                  {elem[index]?.value?.map((elem, ind) => {
+                    return (
+                      <Card id={elem.id} title={elem.title} tag={elem.tag} />
+                    );
+                  })}
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </div>
+    )
+  );
+};
+
+export default DashView;
